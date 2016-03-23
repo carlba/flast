08f19a78dc5d46d5ff049e54d12cbd70b917a7ad
Carl Backstrom <genzorg@gmail.com>
2016-03-24 00:02:47 +0100

    Added initial setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='flast',
+    version='0.1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='Carl Backstrom',
+    author_email='genzorg@gmail.com',
+    description='A simple flask application'
+)
